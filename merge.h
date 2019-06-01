a51c48a470c601f9b31db174c9cd885f21601153
NecroButcher1 <49585659+NecroButcher1@users.noreply.github.com>
2019-06-02 03:10:44 +0600

    release

--- merge.h
+++ merge.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 template<class T>
-const char* merge(deque<T> buff,deque<T> &res){
+const char* merge(deque<T> &buff,deque<T> &res){
     deque<T> tmp=buff;
     res.doEmpty();
     const char *out=NULL;
@@ -12,7 +12,7 @@ const char* merge(deque<T> buff,deque<T> &res){
     if(tmp.empty())out="empty deque";
     else{
         while(!tmp.empty()){
-            tmp.pop_back(value);
+            tmp.pop_front(value);
             res.push_back(value);
             if(!tmp.empty()){
                 tmp.pop_front(value);
